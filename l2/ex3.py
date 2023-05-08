070cf656ac51b6120a8f9f5c58ab635be65864dc
ssf! <123976455+sakilfe@users.noreply.github.com>
2023-05-07 22:07:25 -0300

    code classes

---
+++ l2/ex3.py
@@ -0,0 +1,11 @@
+print("digite a nota do primeiro bimestre da disciplina:")
+n1 = int(input())
+print("digite a nota do segundo bimestre da disciplina:")
+n2 = int(input())
+
+p1 = 2
+p2 = 3
+
+m = ((n1 * p1) + (n2 * p2))//5
+
+print(f'média parcial = {m}')
